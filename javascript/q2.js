6c6b07a7d30af09607a9308eb2c3bcc0291657db
Jonah R. Huggins <jonahrileyhuggins@gmail.com>
2023-11-01 17:49:03 -0400

    modified:   dashboard.html 	modified:   javascript/q2.js

--- javascript/q2.js
+++ javascript/q2.js
@@ -4,60 +4,73 @@
     Answer the following question: How does the profitability of a film relate to the viewer ratings of the film?
 */
 
-// Assuming you have loaded the data from 'revenueVsratings.csv' into a variable 'data'
+d3.csv("revenueVsRatings.csv").then(function(data) {
+  // Define dimensions
+  const dimensions = {
+  width: 800,
+  height: 400,
+  margin: {
+      top: 20,
+      right: 5,
+      bottom: 20,
+      left: 60,
+      },
+    };
+  
 
-// Set the dimensions and margins for the plot
-const margin = { top: 20, right: 30, bottom: 40, left: 50 };
-const width = 800 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+  // Set the dimensions and margins for the plot
+  const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+  const width = 800 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
 
-// Append an SVG element to your HTML
-const svg = d3.select("body")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+  // Append an SVG element to your HTML
+  const svg = d3.select("#scatter-plot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Scale for the x-axis (revenue)
-const xScale = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.revenue)])
-  .range([0, width]);
+  // Scale for the x-axis (revenue)
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.revenue)])
+    .range([0, width]);
 
-// Scale for the y-axis (ratings)
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.rating)])
-  .range([height, 0]);
+  // Scale for the y-axis (ratings)
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.rating)])
+    .range([height, 0]);
 
-// Create circles for each data point
-svg.selectAll("circle")
-  .data(data)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xScale(d.revenue))
-  .attr("cy", d => yScale(d.rating))
-  .attr("r", 5)
-  .attr("fill", "blue");
+  // Create circles for each data point
+  svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.revenue))
+    .attr("cy", d => yScale(d.rating))
+    .attr("r", 5)
+    .attr("fill", "blue");
 
-// Add x-axis
-svg.append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(d3.axisBottom(xScale));
+  // Add x-axis
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
 
-// Add y-axis
-svg.append("g")
-  .call(d3.axisLeft(yScale));
+  // Add y-axis
+  svg.append("g")
+    .call(d3.axisLeft(yScale));
 
-// Add axis labels
-svg.append("text")
-  .attr("x", width / 2)
-  .attr("y", height + 30)
-  .style("text-anchor", "middle")
-  .text("Revenue");
+  // Add axis labels
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height + 30)
+    .style("text-anchor", "middle")
+    .text("Revenue");
 
-svg.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("x", -height / 2)
-  .attr("y", -40)
-  .style("text-anchor", "middle")
-  .text("Ratings");
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -40)
+    .style("text-anchor", "middle")
+    .text("Ratings");
+  });
