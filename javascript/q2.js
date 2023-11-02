b2a6b14d659545785cf7c76dda24c1096bacad14
Jonah R. Huggins <jonahrileyhuggins@gmail.com>
2023-11-02 12:30:19 -0400

    Simplified plotting to potentially solve errors

--- javascript/q2.js
+++ javascript/q2.js
@@ -18,59 +18,76 @@ d3.csv("revenueVsRatings.csv").then(function(data) {
     };
   
 
-  // Set the dimensions and margins for the plot
-  const margin = { top: 20, right: 30, bottom: 40, left: 50 };
-  const width = 800 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
+  // // Set the dimensions and margins for the plot
+  // const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+  // const width = 800 - margin.left - margin.right;
+  // const height = 400 - margin.top - margin.bottom;
 
   // Append an SVG element to your HTML
   const svg = d3.select("#scatter-plot")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .style("width", dimensions.width)
+    .style("height", dimensions.height)
 
-  // Scale for the x-axis (revenue)
-  const xScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.revenue)])
-    .range([0, width]);
+  const xAccessor = d => d.revenue;
+  const yAccessor = d => d.rating;
+    
+    // .append("svg")
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
+    // .append("g")
+    // .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // Scale for the y-axis (ratings)
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.rating)])
-    .range([height, 0]);
-
-  // Create circles for each data point
-  svg.selectAll("circle")
+  const dots = svg.append("g")
+    .selectAll("circle")
     .data(data)
     .enter()
     .append("circle")
-    .attr("cx", d => xScale(d.revenue))
-    .attr("cy", d => yScale(d.rating))
-    .attr("r", 5)
-    .attr("fill", "blue");
+    .attr("cx", d => xScale(xAccessor(d)))
+    .attr("cy", d => yScale(yAccessor(d)))
+    .attr("fill", "black")
+    .attr("r", 3)
 
-  // Add x-axis
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale));
 
-  // Add y-axis
-  svg.append("g")
-    .call(d3.axisLeft(yScale));
+  // Scale for the x-axis (revenue)
+  // const xScale = d3.scaleLinear()
+  //   .domain(d3.extend(data, xAccessor))
+  //   .range([0, dimensions.width]);
 
-  // Add axis labels
-  svg.append("text")
-    .attr("x", width / 2)
-    .attr("y", height + 30)
-    .style("text-anchor", "middle")
-    .text("Revenue");
+  // // Scale for the y-axis (ratings)
+  // const yScale = d3.scaleLinear()
+  //   .domain(d3.extend(data, yAccessor)
+  //   .range([dimensions.height, 0])
 
-  svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("x", -height / 2)
-    .attr("y", -40)
-    .style("text-anchor", "middle")
-    .text("Ratings");
+  // // Create circles for each data point
+  // svg.selectAll("circle")
+  //   .data(data)
+  //   .enter()
+  //   .append("circle")
+  //   .attr("cx", d => xScale(d.revenue))
+  //   .attr("cy", d => yScale(d.rating))
+  //   .attr("r", 5)
+  //   .attr("fill", "blue");
+
+  // // Add x-axis
+  // svg.append("g")
+  //   .attr("transform", `translate(0, ${height})`)
+  //   .call(d3.axisBottom(xScale));
+
+  // // Add y-axis
+  // svg.append("g")
+  //   .call(d3.axisLeft(yScale));
+
+  // // Add axis labels
+  // svg.append("text")
+  //   .attr("x", width / 2)
+  //   .attr("y", height + 30)
+  //   .style("text-anchor", "middle")
+  //   .text("Revenue");
+
+  // svg.append("text")
+  //   .attr("transform", "rotate(-90)")
+  //   .attr("x", -height / 2)
+  //   .attr("y", -40)
+  //   .style("text-anchor", "middle")
+  //   .text("Ratings");
   });
